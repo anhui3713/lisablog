51a7a723ae7a40ba351b77340c5f9282313aab55
anhui3713 <90683@163.com>
2015-09-13 12:09:06 +0800

    build fore module

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+/* 
+ *  配置requirejs的路由表
+ */
+require.config({
+	// 模块加载的其实路径
+	"baseUrl": "/",
+	// 模块及url映射
+	paths: {
+		"jquery": "jquery/dist/jquery.min",
+		"bootstrap": "bootstrap/dist/js/bootstrap.min"
+	},
+	// 非AMD模块之间的相互依赖
+	shim: {
+		// bootstrap依赖于jquery
+		"bootstrap": {
+			"deps": ["jquery"]
+		}
+	}
+});
+
+// 所有网页上引入bootstrap
+require(["bootstrap"]);
