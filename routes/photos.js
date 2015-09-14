56229d0d5fb785bcc51f86f092decf828b9952b6
anhui713 <90683@163.com>
2015-09-15 00:16:28 +0800

    photos view desgin

--- routes/photos.js
+++ routes/photos.js
@@ -1,11 +1,33 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 var mrender = require("./renderer.js");
 
-/* GET users listing. */
+/**
+ * 获取照片列表
+ */
 router.get('/', function(req, res, next) {
   //res.send('respond with photos.')
   mrender.render("photos", req, res);
+
+});
+
+/**
+ * 获取一张照片
+ */
+router.get("/photo/:datefolder/:photoname", function (req, res) {
+  var path = process.cwd().replace(/bin$/, "") + "/photofiles/" + req.params.datefolder + "/" + req.params.photoname;
+
+  fs.readFile(path, "binary", function (error, file) {
+    if(error) {
+      console.log(error);
+      return ;
+    }
+
+    res.writeHead(200, {"Content-Type": "image/png"});
+    res.write(file, "binary");
+    res.end();
+  });
 
 });
 
